707ef913c9189656fc9b14b98fd86d9b9545bccf
Senai Full Stack <senaifullstack2023@gmail.com>
2023-04-15 21:11:29 -0300

    segue

---
+++ js/jquery-script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#botao-cadastrar").click(function(){
+
+    $("#section-login").slidetoggle("slow");
+    $("#section-login").slidetoggle("slow");
+    $("#botao-cadastrar").hide();    
+    });
+
+    }); 
+
+    function cadastro() {
+        alert("cadastro com sucesso!");
+        window.location.href = "index.html";
+
+    }
+
